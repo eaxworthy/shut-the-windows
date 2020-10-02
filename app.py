56d5d6d1d7c09cf6e294b24a5ce602c07c8b82a6
eaxworthy <lizzieaxworthy@gmail.com>
2020-10-01 19:10:06 -0700

    text message format finalized, changed config to have a list of users

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 import requests
-from config import sid, auth_token, sensor
+import time
+from datetime import datetime
+from config import *
 from twilio.rest import Client
 
 client = Client(sid, auth_token)
 
-def send_message(is_safe, aqi_value):
+def send_message(is_safe, aqi_value, number):
 
 	if is_safe:
-		client.messages.create(to="+14152997994", from_='+12056795299', body=f'Air quality has reached an acceptable level of {aqi_value}.')
+		client.messages.create(to=number, from_='+12056795299', body=f'Air quality has reached an acceptable level of {aqi_value}.')
 	else:
-		client.messages.create(to="+14152997994", from_="+12056795299", body=f'Warning: air quality is at {aqi_value}.')
+		client.messages.create(to=number, from_="+12056795299", body=f'Warning: air quality is at {aqi_value}.')
 
 def check_quality():
 	url = 'https://www.purpleair.com/json?show=' + sensor
 	air_data = requests.get(url).json()
 	pm_value = float(air_data['results'][0]['PM2_5Value'])
-	print(pm_to_aqi(pm_value))
+	aqi = pm_to_aqi(pm_value)
+	return aqi, aqi <= 140
 
 def pm_to_aqi(pm_value):
 
@@ -43,4 +46,16 @@ def pm_to_aqi(pm_value):
 		return calc_aqi(pm_value, 100, 51, 35.4, 12.1)
 	else: 
 		return calc_aqi(pm_value, 50, 0, 12, 0)
-check_quality()
+
+
+is_safe = True
+log = 'log_file.txt'
+while True:
+	aqi, current = check_quality()
+	with open(log, 'w') as log:
+		log.write(datetime.now().strftime('%b-%d-%y %H:%M, ') + str(aqi) + '\n')
+	if is_safe != current:
+		is_safe = current
+		for user in users.values():
+			send_message(is_safe, aqi, user)
+	time.sleep(900)
